f38011972be302f6e9dd23e5315868d910be211c
Golik Anton <Golik.a.b@yandex.ru>
2025-04-28 13:29:05 +0300

    1 ревью

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,19 +2,143 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useEffect, useRef, useState } from 'react';
+import {
+	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+	OptionType,
+} from 'src/constants/articleProps';
+import clsx from 'clsx';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	setCurrentArticleState: (param: ArticleStateType) => any;
+	currentArticleState: ArticleStateType;
+};
+
+type UseOutsideClickClose = {
+	isOpen: boolean;
+	onChange: (newValue: boolean) => void;
+	onClose?: () => void;
+	rootRef: React.RefObject<HTMLDivElement>;
+};
+
+const useOutsideClickClose = ({
+	isOpen,
+	rootRef,
+	onClose,
+	onChange,
+}: UseOutsideClickClose) => {
+	useEffect(() => {
+		const handleClick = (event: MouseEvent) => {
+			event.stopPropagation();
+			const { target } = event;
+			if (target instanceof Node && !rootRef.current?.contains(target)) {
+				isOpen && onClose?.();
+				onChange?.(false);
+			}
+		};
+
+		window.addEventListener('mousedown', handleClick);
+
+		return () => {
+			window.removeEventListener('mousedown', handleClick);
+		};
+	}, [onClose, onChange, isOpen]);
+};
+
+export const ArticleParamsForm = ({
+	currentArticleState,
+	setCurrentArticleState,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const rootRef = useRef<HTMLDivElement>(null);
+	const [selectArticleState, setSelectArticleState] =
+		useState<ArticleStateType>(currentArticleState);
+
+	const handleChange = (key: keyof ArticleStateType, value: OptionType) => {
+		setSelectArticleState({ ...selectArticleState, [key]: value });
+	};
+
+	const handleClick = () => {
+		setIsOpen((prevState) => !prevState);
+	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+	});
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
+		<div ref={rootRef}>
+			<ArrowButton isOpen={isOpen} onClick={handleClick} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form
+					className={clsx(styles.form)}
+					onSubmit={(event) => {
+						event.preventDefault();
+						setCurrentArticleState(selectArticleState);
+						setIsOpen(false);
+					}}
+					onReset={() => {
+						setCurrentArticleState(defaultArticleState);
+						setSelectArticleState(defaultArticleState);
+						setIsOpen(false);
+					}}>
+					<Select
+						selected={selectArticleState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => handleChange('fontFamilyOption', option)}
+						title='Шрифт'
+					/>
+
+					<RadioGroup
+						name='radio'
+						selected={selectArticleState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) => handleChange('fontSizeOption', option)}
+						title='Размер шрифта'
+					/>
+
+					<Select
+						selected={selectArticleState.fontColor}
+						options={fontColors}
+						onChange={(option) => handleChange('fontColor', option)}
+						title='Цвет шрифта'
+					/>
+
+					<Separator />
+
+					<Select
+						selected={selectArticleState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => handleChange('backgroundColor', option)}
+						title='Цвет фона'
+					/>
+
+					<Select
+						selected={selectArticleState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => handleChange('contentWidth', option)}
+						title='Ширина контента'
+					/>
+
+					<div className={clsx(styles.bottomContainer)}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
